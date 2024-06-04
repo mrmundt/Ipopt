a3bff2bb3c43eaee71ebd58fe1c0b9bc03cc43c6
Daniel Doehring <doehringd2@gmail.com>
2024-06-04 14:01:07 +0200

    use conventional naming of arguments to `main()` (#769)

--- contrib/sIPOPT/AmplSolver/ampl_sipopt.cpp
+++ contrib/sIPOPT/AmplSolver/ampl_sipopt.cpp
@@ -12,8 +12,8 @@
 #include "SensRegOp.hpp"
 
 int main(
-   int    /*argv*/,
-   char** argc
+   int    /*argc*/,
+   char** argv
 )
 {
    using namespace Ipopt;
@@ -123,7 +123,7 @@ int main(
                                     "Number of sensitivity steps");
 
    // create AmplSensTNLP from argc.
-   SmartPtr<TNLP> sens_tnlp = new SensAmplTNLP(ConstPtr(app_ipopt->Jnlst()), app_ipopt->RegOptions(), app_ipopt->Options(), argc, suffix_handler,
+   SmartPtr<TNLP> sens_tnlp = new SensAmplTNLP(ConstPtr(app_ipopt->Jnlst()), app_ipopt->RegOptions(), app_ipopt->Options(), argv, suffix_handler,
       false, ampl_options_list);
 
    app_sens->Initialize();
